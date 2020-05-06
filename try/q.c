cd9fd7edccb63840608af193ad1321d494eccf6c
Leonardo Malcotti <leonardo.malcotti@studenti.unitn.it>
2020-05-06 17:54:19 +0200

    aggiornato q e aggiunto un file per i test

--- try/q.c
+++ try/q.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 /*
 prende in input un file .txt e scrive un file qnm.txt
@@ -13,19 +14,48 @@ ascii ricorrenza \n
 
 */
 
+void print_help() {
+    printf("\n");
+    printf("parametri\n\n");
+    printf("-f  indica il percorso del file di input.\n");
+    printf("-n  indica il numero del gruppo di file.\n");
+    printf("    se non specificato n sarà 0 di default.\n");
+    printf("-m  indica il numero di parti in cui il file è da suddividere.\n");
+    printf("    se non specificato sarà 0, quindi verrà analizzato tutto il file.\n");
+    printf("-c  indica quale parte analizzare, se a 0 analizza tutto il file.\n");
+    printf("    se non specificato sarà 0.\n");
+    printf("\n");
+}
+
+#define ARG_N 0
+#define ARG_M 1
+#define ARG_C 2
+#define ARG_F 3
+
 int main(int argn, char *argv[]){
-    int i;//indice per i for
-    int n = 0;
-    int m = 0;
-    FILE *finput = NULL;
+    int i, n, m, c = 0;
+    int finput;
+    int contr_arg[4] = {0,0,0,0};
+    int len_file = 0;
+
 
     //primi controlli sugli argomenti
     if(argn == 1){
         printf("argomento input non trovato\n");
+        print_help();
         return 0;
     }
-    if(argn>7){
+    if(argn>9){
         printf("troppi argomenti specificati\n");
+        print_help();
+        return 0;
+    }
+
+    //controlla che ci sia il numero giusto di argomenti
+    if(argn%2 == 0){
+        //inserire istruzioni sul comando
+        printf("erroneo utilizzo degli argomenti 1\n");
+        print_help();
         return 0;
     }
 
@@ -35,30 +65,45 @@ int main(int argn, char *argv[]){
 
     for(i=1;i<argn;i+=2){
 
-        //controlla che ci sia il numero giusto di argomenti
-        if(i+1>=argn){
-            //inserire istruzioni sul comando
-            printf("erroneo utilizzo degli argomenti 1\n");
-            return 0;
-        }
-
         //apre il file indicato nei parametri
         //gestisce eventuali errori
         if(strcmp(argv[i],"-f")==0){
 
-            finput = fopen(argv[i+1],"r+");
+            //controlla che l'argomento non sia stato già usato
+            if(contr_arg[ARG_F]!= 0){
+                printf("hai usato -f troppe volte\n");
+                print_help();
+                return 0;
+            } else {
+                contr_arg[ARG_F] = 1;
+            }
 
-            if(finput == NULL){
+            finput = open(argv[i+1],O_RDWR);
+
+            if(finput < 0){
                 //
                 //inserire controllo errori
                 //
                 printf("errore nel file passato\n");
+                print_help();
+                return 0;
             }
+            len_file = lseek(finput,0,SEEK_END);
+            lseek(finput,0,SEEK_SET);
         }
 
         //legge dai parametri il valore di n
         //controllando che l'input si effettivamente un numero
         if(strcmp(argv[i],"-n") == 0){
+
+            if(contr_arg[ARG_N]!= 0){
+                printf("hai usato -n troppe volte\n");
+                print_help();
+                return 0;
+            } else {
+                contr_arg[ARG_N] = 1;
+            }
+
             char *err;
             long int tmp = strtol(argv[i+1],&err,10);
             if(strcmp(err,"")!=0){
@@ -66,6 +111,7 @@ int main(int argn, char *argv[]){
                 //inserire istruzioni sul comando
                 //
                 printf("erroneo utilizzo degli argomenti 2\n");
+                print_help();
                 return 0;
             }
             n=(int)tmp;
@@ -74,6 +120,15 @@ int main(int argn, char *argv[]){
         //legge dai parametri il valore di m
         //controllando che l'input si effettivamente un numero
         if(strcmp(argv[i],"-m") == 0){
+
+            if(contr_arg[ARG_M]!= 0){
+                printf("hai usato -m troppe volte\n");
+                print_help();
+                return 0;
+            } else {
+                contr_arg[ARG_M] = 1;
+            }
+
             char *err;
             long int tmp = strtol(argv[i+1],&err,10);
             if(*err){
@@ -81,23 +136,86 @@ int main(int argn, char *argv[]){
                 //inserire istruzioni sul comando
                 //
                 printf("erroneo utilizzo degli argomenti 3\n");
+                print_help();
                 return 0;
             }
             m=(int)tmp;
         }
+
+        //legge dai parametri il valore di c
+        //controllando che l'input si effettivamente un numero
+        if(strcmp(argv[i],"-c") == 0){
+
+            if(contr_arg[ARG_C]!= 0){
+                printf("hai usato -c troppe volte\n");
+                print_help();
+                return 0;
+            } else {
+                contr_arg[ARG_C] = 1;
+            }
+
+            char *err;
+            long int tmp = strtol(argv[i+1],&err,10);
+            if(*err){
+                //
+                //inserire istruzioni sul comando
+                //
+                printf("erroneo utilizzo degli argomenti 4\n");
+                print_help();
+                return 0;
+            }
+            c=(int)tmp;
+
+            if(c > m || c<0){
+                printf("c deve essere un intero positivo minore di m\n");
+                print_help();
+                return 0;
+            }
+        }
     }
+
+    //
+    //Analisi del file
+    //
 
     //inorridisco ma intanto ok
     int caratteri[256]={[0 ... 255]=0};
 
-    char rff;
-    while(fscanf(finput,"%c",&rff)!=EOF){
-        //printf("%c",input[i]);
-        caratteri[rff]++;
+    char *rff = (char *)calloc(len_file,sizeof(char));
+
+    int rd=0;
+
+
+    if(c==0 || m==0){
+        //legge tutto il file
+        for(i=0;i<len_file;i++){
+            rd = read(finput,rff,1);
+            //printf("%d\n",rd);
+            if(rd!=0){
+                //printf("%c",rff[i]);
+                caratteri[rff[i]]++;
+            } else{
+                //printf("fine\n");
+            }
+        }
+    } else{
+        int len_parti = len_file/m;
+        lseek(finput,(c-1)*len_parti,SEEK_SET);
+
+        rd = read(finput,rff,len_parti);
+
+        if(rd>0){
+            for(i=0;i<len_parti;i++){
+                
+                caratteri[rff[i]]++;
+            }
+        }
     }
 
-    fflush(finput);
-    fclose(finput);
+
+    //fflush(finput);
+
+    close(finput);
 
     //nome del file di output
     char outName[12];
