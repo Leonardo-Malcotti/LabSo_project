05b3edcef29e10596ab376dfc5ccd41c7d4686cc
Filippo Fanton <fantonfilippo@gmail.com>
2020-05-22 11:30:27 +0200

    Piccole correzioni su r.c

--- try/r.c
+++ try/r.c
@@ -8,7 +8,7 @@
 #define COUNT 1 //Quanti byte leggere ad ogni read
 /*
 prende in input un file .txt e ne stampa a video il report, fornendo statistiche.
-il formato del file di input Ã¨:
+il formato del file di input è:
 
 ascii ricorrenza \n
 
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
     int *buff;      //Buffer in cui salvo i dati letti
 
 //Step 1: Gestione dell'argomento in ingresso e creazione del File Descriptor
-    
+
     if(argc == 1){
         printf("argomento input non trovato\n");
         print_r_help();
@@ -32,13 +32,13 @@ int main(int argc, char *argv[]) {
         print_r_help();
         exit(-1);
     }
-    
+
     int contr_arg[4] = {1,1,0,1}; //Accetto solo un argomento di tipo -f
     for(i=1;i<argc;i++){
        //apre il file indicato nei parametri
         //gestisce eventuali errori
         if(param_check(argv[i],ARG_F,contr_arg) == 0){
-            controllo = 1;
+            //controllo = 1;
             if((finput = open_file(argv[i+1],&len_file)) == -1){
                 //stampa errori
                 exit(-1);
@@ -47,13 +47,13 @@ int main(int argc, char *argv[]) {
     }
 
 //Step 2: lettura da finput
-    
+
     while(read(finput, buff, COUNT)>0){
         printf("%d\n", buff); //Per capire quanto legge, TEMPORANEO
     }
 
 
-    
+
 
     close(finput);
     return 0;
